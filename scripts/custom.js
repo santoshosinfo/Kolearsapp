dafd07d5ecd8793ebd7ee280fe75fc87a9d115bd
santoshosinfo <santosh@osinfotech.com>
2015-07-04 20:54:46 +0530

    url chamges

--- scripts/custom.js
+++ scripts/custom.js
@@ -27,9 +27,9 @@
 
 
 $(document).ready(function(){
-//var serviceURL = "http://k2s2c.in/kolears/services/";
+var serviceURL = "http://k2s2c.in/kolears/services/";
 //var serviceURL = "http://santosh.phpzeal.com/kolears/";
-var serviceURL = "http://localhost/android/kolears/";
+//var serviceURL = "http://localhost/android/kolears/";
 
 var category;
 var sub_cat;
@@ -47,7 +47,7 @@ $('#catlist').on('click','.maincat',function()
 getcategorylist();
 function getcategorylist() {
 var subcat_html;
-var cat_url ='http://localhost/android/Kolearsapp/';
+//var cat_url ='http://localhost/android/Kolearsapp/';
 var cat_url='http://k2s2c.in/kolears/services/details.php?id=';
 	$.getJSON(serviceURL + 'category.php', function(data) {
 	//alert(data);
