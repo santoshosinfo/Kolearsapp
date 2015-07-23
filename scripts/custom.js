9c9dce3db2d2f76f242b0ebc3d827f8b19a37951
santoshosinfo <santosh@osinfotech.com>
2015-07-23 16:32:30 +0530

    23july

--- scripts/custom.js
+++ scripts/custom.js
@@ -24,6 +24,28 @@
 		  },false);
 	  }
   }
+function str_repeat(input, multiplier) {
+  //  discuss at: http://phpjs.org/functions/str_repeat/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  // improved by: Ian Carter (http://euona.com/)
+  //   example 1: str_repeat('-=', 10);
+  //   returns 1: '-=-=-=-=-=-=-=-=-=-='
+
+  var y = '';
+  while (true) {
+    if (multiplier & 1) {
+      y += input;
+    }
+    multiplier >>= 1;
+    if (multiplier) {
+      input += input;
+    } else {
+      break;
+    }
+  }
+  return y;
+}
 
 
 $(document).ready(function(){
@@ -41,9 +63,107 @@ $('#catlist').on('click','.maincat',function()
 	//alert('hi');
 	$(this).next('.sub-menu-one').toggle();
 });
+$('#catlist1').on('click','.maincat',function()
+{
+	//alert('hi');
+	$(this).next('.sub-menu-one').toggle();
+});
+
+$(".sub-menu-one").each( function() {
+	var innerHtml = $( this ).html();
+	if( innerHtml != '' )
+	{
+	$(this).prev('a').attr('href','#');
+	}
+	else
+	{
+	$(this).prev('a').find('strong').remove();
+	}
+} );
+
 
 getcategorylist();
-function getcategorylist() {
+
+function removehref()
+{
+	$("#catlist .sub-menu-one").each( function() {
+	var innerHtml = $( this ).html();
+	if( innerHtml != '' )
+	{
+	$(this).prev('a').attr('href','#');
+	
+	}
+	else
+	{
+	$(this).prev('a').find('strong').remove();
+	}
+	
+} );
+}
+function getcategorylist()
+{
+var cat_html;
+var sub_cat_html;
+var catg;
+var catname;
+var cat_order = [];
+var counter;
+var l_prefix='';
+
+//var cat_url ='http://localhost/android/Kolearsapp/details.php?id=';
+var cat_url='http://k2s2c.in/kolears/services/app/details.php?id=';
+$.getJSON(serviceURL + 'category.php', function(data) {
+category = data.items;
+//console.log(category);
+//var main_html=;
+var div_start = false;
+//var divmain_html='';
+//alert(category.length);
+var main_html ='';
+for (var i = 0; i < category.length; i++)
+{
+	 
+	 var catg = category[i];
+	
+	 var mfinal = catg.cat_name;
+	//console.log(mfinal);
+	
+	
+	
+	 if( catg.parent_cat_id==0 && div_start == false )
+	 {
+	 main_html += '<a class="menu-item menu-icon img maincat menucheck " id="sub-menu-one" href="'+cat_url+catg.cat_id+'">'+mfinal+'<strong></strong></a><div class="sub-menu-one" style="display:none;">';
+	 div_start = true;
+	 
+		
+	 }
+	 else{
+	 
+	  main_html += '<a class="sub-menu-item"  href="'+cat_url+catg.cat_id+'-'+catg.parent_cat_id+'">'+mfinal+'</a>';
+	 }
+	//console.log(category[i+1].parent_cat_id);
+	 if( i+1 < category.length && category[i+1].parent_cat_id==0 )
+		{
+			 main_html += '</div>';
+			div_start = false;
+		}
+		
+		//console.log(main_html);
+	 
+	 
+}
+//console.log('final');
+//console.log(main_html);
+$('#catlist').html(main_html);
+removehref();
+
+	
+
+
+});
+	
+}
+/*function getcategorylist() {
 	
 var cat_html;
 var sub_cat_html;
@@ -52,8 +172,8 @@ var catname;
 var cat_order = [];
 var counter;
 
-//var cat_url ='http://localhost/android/Kolearsapp/details.php?id=';
-var cat_url='http://k2s2c.in/kolears/services/app/details.php?id=';
+var cat_url ='http://localhost/android/Kolearsapp/details.php?id=';
+//var cat_url='http://k2s2c.in/kolears/services/app/details.php?id=';
 	$.getJSON(serviceURL + 'category.php', function(data) {
 	//alert(data);
 	category = data.items;
@@ -159,7 +279,7 @@ var cat_url='http://k2s2c.in/kolears/services/app/details.php?id=';
 		
 	});
 
-}
+}*/
 /*function getprodcutdetails()
 {
 	var imgURL = "http://localhost/tgc1/uploads/";
@@ -377,7 +497,7 @@ function getUrlVars() {
 	  });	
 
 	
-	$('#sub-menu-one').click(function(){$('.sub-menu-one').toggle(200); return false;});
+	/*$('#sub-menu-one').click(function(){$('.sub-menu-one').toggle(200); return false;});*/
 	$('#sub-menu-two').click(function(){$('.sub-menu-two').toggle(200); return false;});
 	$('#sub-menu-three').click(function(){$('.sub-menu-three').toggle(200); return false;});
 	$('#sub-menu-four').click(function(){$('.sub-menu-four').toggle(200); return false;});
